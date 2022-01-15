cc12e1633fd4bdd6895484bfb7b7c8cf3e9edfb3
Joshua Mankelow <joshua@mankelow.org>
2022-01-15 17:20:26 +0000

    Added functions to clean csv data exported from ggLeap admin panel. Will update this as I find more issues with ggLeap data exporting

--- src/ggLeap/helpers.py
+++ src/ggLeap/helpers.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from datetime import datetime, date
 
 def remove_date_datetime(dt: date) -> date:
@@ -8,5 +10,37 @@ def remove_date_datetime(dt: date) -> date:
 
 def ggLeap_str_to_datetime(string: str) -> date:
     date = datetime.strptime(string,
-                             "%m/%d/%Y %I:%M:%S %p")
+                             '%m/%d/%Y %I:%M:%S %p')
     return date
+
+def read_csv(path: str) -> pd.DataFrame:
+    """ Function to replace pandas.read_csv() because
+    ggLeap data is screwy to start with so this function
+    cleans the data and then loads it into a pandas
+    dataframe """
+    cleaned_csv = clean_csv(path)
+    return cleaned_csv
+
+def clean_csv(path: str) -> pd.DataFrame:
+    f = open(path, 'r')
+    data = f.readlines()
+
+    new_df_vals = []
+    for datum in data:
+        values = datum.split(',')
+
+        if 'RemovedOffer' in values:
+            # merge "Offer name: XXXXXX" and "Reason: XXXXX"
+            index = values.index('RemovedOffer')
+            offer_name = values[index + 1]
+            reason = values[index + 2]
+
+            new_val = offer_name + "; " + reason
+
+            values.pop(index + 2)
+            values.pop(index + 1)
+            values.insert(index + 1, new_val)
+
+        new_df_vals.append(values)
+
+    return pd.DataFrame(new_df_vals)
