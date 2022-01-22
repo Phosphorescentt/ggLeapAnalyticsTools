75d414a37e759a9f06d374241ec45068e6f85325
Joshua Mankelow <joshua@mankelow.org>
2022-01-22 21:45:18 +0000

    Used .copy() methods to make sure that there's no data leakage

--- src/ggLeap/data_aggregation.py
+++ src/ggLeap/data_aggregation.py
@@ -13,38 +13,44 @@ def remove_dates(df: pd.DataFrame) -> pd.DataFrame:
     
     new_records = []
     for i in range(len(df)):
-        current_record = df.loc[i]
+        current_record = df.loc[i].copy()
         date_string = current_record["Date"]
         dt = helpers.ggLeap_str_to_datetime(date_string)
         new_dt = helpers.remove_date_datetime(dt)
         current_record["Date"] = new_dt
-        new_records.append(current_record)
+        new_records.append(current_record.copy())
 
     return pd.DataFrame(new_records)
 
 
 def remove_weeks(df: pd.DataFrame) -> pd.DataFrame:
-    """ Remove all weeks from datetimes
+    """
+        Remove all weeks from datetimes
 
         Change all dates in the DataFrame to the following:
+        DD/MM/YY
         01/01/01 for a Monday
         02/01/01 for a Tuesday
         03/01/01 for a Wednesday
         04/01/01 for a Thursday
         05/01/01 for a Friday
         06/01/01 for a Saturday
-        07/01/01 for a Sunday """
+        07/01/01 for a Sunday
+    """
 
+    days = [0 for i in range(7)]
     new_records = []
     for i in range(len(df)):
-        current_record = df.loc[i]
+        current_record = df.loc[i].copy()
         date_string = current_record["Date"]
         dt = helpers.ggLeap_str_to_datetime(date_string)
-        new_dt = helpers.remove_week_datetime(dt)
+        # print(dt.today().weekday())
+        new_dt, day = helpers.remove_week_datetime(dt)
+        days[day] += 1
         current_record["Date"] = new_dt
-        new_records.append(current_record)
+        new_records.append(current_record.copy())
 
-    return pd.DataFrame(new_records)
+    return pd.DataFrame(new_records.copy()), days
 
 
 def total_user_seconds(dh: DataHandler.DataHandler, 
