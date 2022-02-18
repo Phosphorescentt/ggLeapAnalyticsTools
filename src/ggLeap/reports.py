aa67b321bb4af1b362f1e1d4f88a057889c55054
Joshua Mankelow <joshua@mankelow.org>
2022-02-18 16:02:29 +0000

    #2 implemented but still waiting on some other fixes (e.g. #2) before PRing into main

--- src/ggLeap/reports.py
+++ src/ggLeap/reports.py
@@ -30,5 +30,61 @@ def concurrent_logins(df: pd.DataFrame) -> pd.DataFrame:
     )
 
     # Combine the two to get concurrent logins
-    # Done?
-    pass
+    if login_days != logout_days:
+        raise Exception("Unable to parse data.")
+
+    distns: list[tuple[list, list]] = []
+    for i in range(len(login_days)):
+        login_distn_current = login_distns[i]
+        logout_distn_current = logout_distns[i]
+
+        login_times = login_distn_current[0]
+        logout_times = logout_distn_current[0]
+
+        total = len(login_times) + len(logout_times)
+
+        print(f"Len login times: {len(login_times)}")
+        print(f"Len logout times: {len(logout_times)}")
+        print(f"Total: {total}")
+
+        temp_cum_actions: tuple[list, list] = ([], [])
+        a = 0
+        b = 0
+        current_users = 0
+        for j in range(total):
+            print(f"j: {j}")
+            print(f"a: {a}")
+            print(f"b: {b}")
+            print("======")
+
+            try:
+                login_time = login_times[a]
+                logout_time = logout_times[b]
+                if login_time < logout_time:
+                    a += 1
+                    current_users += 1
+
+                    temp_cum_actions[0].append(login_time)
+                    temp_cum_actions[1].append(current_users)
+                elif logout_time < login_time:
+                    b += 1
+                    current_users -= 1
+
+                    temp_cum_actions[0].append(logout_time)
+                    temp_cum_actions[1].append(current_users)
+                elif login_time == logout_time:
+                    a += 1
+                    b += 1
+                    current_users += 0
+
+                    temp_cum_actions[0].append(login_time)
+                    temp_cum_actions[1].append(current_users)
+                else:
+                    raise Exception("Something went wrong.")
+            except:
+                temp_cum_actions[0].append(login_time)
+                temp_cum_actions[1].append(current_users)
+
+        distns.append(temp_cum_actions)
+
+    return login_days, distns
